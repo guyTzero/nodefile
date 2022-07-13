8fd3e5d4d392ea16349c96180bbc29bbc8e2c8d3
guy <guyth1997v2@gmail.com>
2022-07-13 15:37:18 +0700

    update crud

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const multer = require('multer');
 const ejs = require('ejs');
 const path = require('path');
 const port = process.env.PORT || 3000;
+const csv = require('csv-parser');
+const fs = require('fs');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 // Set The Storage Engine
 const storage = multer.diskStorage({
@@ -67,6 +70,45 @@ app.get('/file/:name', function (req, res, next) {
 })
 
 app.get('/', (req, res) => res.render('index'));
+
+app.get('/data', function(req, res){
+  let arr = []
+  fs.createReadStream('data.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    console.log(row);
+    arr.push(row)
+  })
+  .on('end', () => {
+    console.log('CSV file successfully processed');
+    res.json({ data: arr });
+  });
+});
+
+app.get('/add:id', function(req, res){
+  const csvWriter = createCsvWriter({
+    path: 'data.csv',
+    header: [
+      {id: 'id', title: 'id'},
+      {id: 'label', title: 'label'},
+    ]
+  });
+  let arr = []
+  fs.createReadStream('data.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    console.log(row);
+    arr.push({id:row.id,label:row.label})
+  })
+  .on('end', () => {
+    arr.push({id:'id',label:req.params.id.replace(":", "")})
+    console.log('CSV file successfully processed');
+    csvWriter
+    .writeRecords(arr)
+    .then(()=> console.log('The CSV file was written successfully'));
+  });
+  console.log('all',arr)
+});
 
 app.post('/upload', (req, res) => {
   upload(req, res, (err) => {
